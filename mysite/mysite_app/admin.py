03ca3190120573cd58652004b20f8ecb3956ae19
unknown <utagarwal687@gmail.com>
2019-01-05 15:55:04 +0530

    first commit

---
+++ mysite/mysite_app/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from mysite_app.models import Post,Comment
+
+# Register your models here.
+admin.site.register(Post)
+admin.site.register(Comment)
---
+++ mysite/mysite_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MysiteAppConfig(AppConfig):
+    name = 'mysite_app'
